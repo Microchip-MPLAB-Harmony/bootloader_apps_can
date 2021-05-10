9798fe680dea834a3b9e7eb04e342ab25d9f2423
Amit Raddi <amit.raddi@microchip.com>
2021-05-10 15:05:09 +0530

    [CAN] Regenerated demo for can_bootloader_bootloader_sam_e54_xpro
    MH3-52741
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/can_bootloader/bootloader/firmware/src/config/sam_e54_xpro/peripheral/can/plib_can1.h
+++ apps/can_bootloader/bootloader/firmware/src/config/sam_e54_xpro/peripheral/can/plib_can1.h
@@ -103,6 +103,8 @@ bool CAN1_TxFIFOIsFull(void);
 void CAN1_MessageRAMConfigSet(uint8_t *msgRAMConfigBaseAddress);
 bool CAN1_StandardFilterElementSet(uint8_t filterNumber, can_sidfe_registers_t *stdMsgIDFilterElement);
 bool CAN1_StandardFilterElementGet(uint8_t filterNumber, can_sidfe_registers_t *stdMsgIDFilterElement);
+void CAN1_SleepModeEnter(void);
+void CAN1_SleepModeExit(void);
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
     }
