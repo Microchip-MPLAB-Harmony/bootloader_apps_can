0fff3ec1736affee43b47b5de058b707b046f6af
Kevin Abishek S <kevinabishek.s@microchip.com>
2025-06-04 15:24:54 +0530

    [CAN_BOOTLOADER] Regenerated demo for can_bootloader__test_app__firmware__pic32cz_ca80_curiosity_ultra.X MH3-112783

--- apps/can_bootloader/test_app/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/port/plib_port.c
+++ apps/can_bootloader/test_app/firmware/src/config/pic32cz_ca80_curiosity_ultra/peripheral/port/plib_port.c
@@ -71,7 +71,9 @@
 void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
+   PORT_REGS->GROUP[0].PORT_PINCFG[9] = 0x1U;
 
+   PORT_REGS->GROUP[0].PORT_PMUX[4] = 0x30U;
 
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x600000U;
@@ -275,6 +277,14 @@ void PORT_GroupToggle(PORT_GROUP group, uint32_t mask)
 void PORT_GroupInputEnable(PORT_GROUP group, uint32_t mask)
 {
     ((port_group_registers_t*)group)->PORT_DIRCLR = mask;
+    
+    for(uint32_t i = 0U; i < 32U; i++)
+    {
+        if((mask & ((uint32_t)1U << i)) != 0U)
+        {
+            ((port_group_registers_t*)group)->PORT_PINCFG[i] |= PORT_PINCFG_INEN_Msk;
+        }
+    }
 }
 
 // *****************************************************************************
