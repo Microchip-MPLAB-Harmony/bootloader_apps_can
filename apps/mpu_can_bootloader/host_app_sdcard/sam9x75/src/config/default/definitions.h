8974a7c0e6ae1b5397b089d377088a52f0dbbfeb
Jignesh Moradiya <Jignesh.Moradiya@microchip.com>
2026-02-18 15:37:34 +0530

    [MPU_CAN_BOOTLOADER] Regenerated demo for apps__mpu_can_bootloader__host_app_sdcard__sam9x75__sam_9x75_curiosity.X
    MH3C-521

--- apps/mpu_can_bootloader/host_app_sdcard/sam9x75/src/config/default/definitions.h
+++ apps/mpu_can_bootloader/host_app_sdcard/sam9x75/src/config/default/definitions.h
@@ -60,14 +60,14 @@
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/aic/plib_aic.h"
 #include "peripheral/sdmmc/plib_sdmmc0.h"
-#include "peripheral/mcan/plib_mcan1.h"
 #include "bsp/bsp.h"
+#include "peripheral/mcan/plib_mcan1.h"
 #include "peripheral/tc/plib_tc0.h"
-#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "system/time/sys_time.h"
 #include "app.h"
 
 
